bd1ac3e96f6ab335d14fe8d163757c803aaf3073
Harsh <harshdlw@gmail.com>
2022-01-07 10:42:05 -0800

    Add Backend Code

---
+++ app/src/main/java/com/example/campusapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.campusapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firstyear = findViewById<Button>(R.id.firstyear)
+        firstyear.setOnClickListener(){
+            val intent= Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+        val secondyear = findViewById<Button>(R.id.secondyear)
+        secondyear.setOnClickListener(){
+            val intent= Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+        val thirdyear = findViewById<Button>(R.id.thirdyear)
+        thirdyear.setOnClickListener(){
+            val intent= Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+        val fourthyear = findViewById<Button>(R.id.fourthyear)
+        fourthyear.setOnClickListener(){
+            val intent= Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
