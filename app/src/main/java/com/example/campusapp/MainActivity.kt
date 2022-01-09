d773ecee6d2e55d19226804cc874677d1c8e497c
Harsh <harshdlw@gmail.com>
2022-01-09 00:31:46 -0800

    bhaukal hai apna

--- app/src/main/java/com/example/campusapp/MainActivity.kt
+++ app/src/main/java/com/example/campusapp/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
                               val intent=Intent(this, HomeActivity::class.java)
             startActivity(intent)
             finish()
-        },3000)
+        },2000)
 
     }
 
