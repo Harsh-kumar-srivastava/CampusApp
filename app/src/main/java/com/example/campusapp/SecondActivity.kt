00f0e5d5446079662e69dfbc08bdf996f30bf40d
Harsh <harshdlw@gmail.com>
2022-01-09 22:22:22 -0800

    Hmlogo ke taraf se ye final app hai sir

--- app/src/main/java/com/example/campusapp/SecondActivity.kt
+++ app/src/main/java/com/example/campusapp/SecondActivity.kt
@@ -1,10 +1,12 @@
 package com.example.campusapp
 
+import android.app.Activity
 import android.content.Intent
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.campusapp.R
-import com.example.campusapp.FourthActivity
+//import com.example.campusapp.FourthActivity
 import android.os.Bundle as Bundle1
 
 open class SecondActivity :AppCompatActivity() {
@@ -13,39 +15,85 @@ open class SecondActivity :AppCompatActivity() {
         setContentView(R.layout.activity_second)
 
         val Cse  = findViewById<Button>(R.id.button10)
+
         Cse.setOnClickListener{
             val checkflag : String? = intent.getStringExtra("flag")
-            val intent=Intent(this, FourthActivity::class.java)
-            val extras=Bundle1()
-            extras.putString("YEAR","1")
-            extras.putString("BRANCH","Cse")
-            intent.putExtras(extras)
-            startActivity(intent)
+
+            if(checkflag.equals("year1")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.fstcse)
+                startActivity(intent)
+
+            }
+           if(checkflag.equals("year2")){
+               val intent=Intent(this, ThirdActivity::class.java)
+               intent.putExtra("picture" , R.drawable.sndcse)
+               startActivity(intent)
+           }
+            if(checkflag.equals("year3")){
+               val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.trdcse)
+               startActivity(intent)
+           }
+            if(checkflag.equals("year4")){
+               val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.frtcse)
+               startActivity(intent)
+           }
 
         }
-
         val Ece  = findViewById<Button>(R.id.button11)
         Ece.setOnClickListener{
             val checkflag : String? = intent.getStringExtra("flag")
-            val intent=Intent(this,FourthActivity::class.java)
-            val extras=Bundle1()
-            extras.putString("YEAR","1")
-            extras.putString("BRANCH","Ece")
-            intent.putExtras(extras)
-            startActivity(intent)
+            if (checkflag.equals("year1")) {
+                val intent = Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.fstme)
+                startActivity(intent)
+            }
 
+            if(checkflag.equals("year2")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.sndece)
+                startActivity(intent)
+            }
+
+            if(checkflag.equals("year3")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.trdece)
+                startActivity(intent)
+            }
+            if(checkflag.equals("year4")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.frtece)
+                startActivity(intent)
+            }
         }
 
 
         val Ee  = findViewById<Button>(R.id.button15)
         Ee.setOnClickListener{
             val checkflag : String? = intent.getStringExtra("flag")
-            val intent=Intent(this,FourthActivity::class.java)
-            val extras=Bundle1()
-            extras.putString("YEAR","1")
-            extras.putString("BRANCH","Ee")
-            intent.putExtras(extras)
-            startActivity(intent)
+            if (checkflag.equals("year1")) {
+                val intent = Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.fstee)
+                startActivity(intent)
+            }
+
+            if(checkflag.equals("year2")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.sndee)
+                startActivity(intent)
+            }
+            if(checkflag.equals("year3")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.trdee)
+                startActivity(intent)
+            }
+            if(checkflag.equals("year4")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.frtee)
+                startActivity(intent)
+            }
 
         }
 
@@ -53,25 +101,54 @@ open class SecondActivity :AppCompatActivity() {
         val Me  = findViewById<Button>(R.id.button13)
         Me.setOnClickListener{
             val checkflag : String? = intent.getStringExtra("flag")
-            val intent=Intent(this,FourthActivity::class.java)
-            val extras=Bundle1()
-            extras.putString("YEAR","1")
-            extras.putString("BRANCH","Me")
-            intent.putExtras(extras)
-            startActivity(intent)
+            if (checkflag.equals("year1")) {
+                val intent = Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.fstme)
+                startActivity(intent)
+            }
 
+            if(checkflag.equals("year2")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.sndme)
+                startActivity(intent)
+            }
+            if(checkflag.equals("year3")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.trdme)
+                startActivity(intent)
+            }
+            if(checkflag.equals("year4")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.frtme)
+                startActivity(intent)
+            }
         }
 
 
         val Ce  = findViewById<Button>(R.id.button16)
         Ce.setOnClickListener{
             val checkflag : String? = intent.getStringExtra("flag")
-            val intent=Intent(this,FourthActivity::class.java)
-            val extras=Bundle1()
-            extras.putString("YEAR","1")
-            extras.putString("BRANCH","Ce")
-            intent.putExtras(extras)
-            startActivity(intent)
+            if (checkflag.equals("year1")) {
+                val intent = Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.fstce)
+                startActivity(intent)
+            }
+
+            if(checkflag.equals("year2")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.sndce)
+                startActivity(intent)
+            }
+            if(checkflag.equals("year3")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.trdce)
+                startActivity(intent)
+            }
+            if(checkflag.equals("year4")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.frtce)
+                startActivity(intent)
+            }
 
         }
 
@@ -79,24 +156,54 @@ open class SecondActivity :AppCompatActivity() {
         val Ipe  = findViewById<Button>(R.id.button12)
         Ipe.setOnClickListener{
             val checkflag : String? = intent.getStringExtra("flag")
-            val intent=Intent(this,FourthActivity::class.java)
-            val extras=Bundle1()
-            extras.putString("YEAR","1")
-            extras.putString("BRANCH","Ipe")
-            intent.putExtras(extras)
-            startActivity(intent)
+            if (checkflag.equals("year1")) {
+                val intent = Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.fstip)
+                startActivity(intent)
+            }
+
+            if(checkflag.equals("year2")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.sndic)
+                startActivity(intent)
+            }
+            if(checkflag.equals("year3")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.trdip)
+                startActivity(intent)
+            }
+            if(checkflag.equals("year4")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.frtip)
+                startActivity(intent)
+            }
 
         }
 
         val Ice  = findViewById<Button>(R.id.button14)
         Ice.setOnClickListener{
             val checkflag : String? = intent.getStringExtra("flag")
-            val intent=Intent(this,FourthActivity::class.java)
-            val extras=Bundle1()
-            extras.putString("YEAR","1")
-            extras.putString("BRANCH","Ice")
-            intent.putExtras(extras)
-            startActivity(intent)
+            if (checkflag.equals("year1")) {
+                val intent = Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.fstic)
+                startActivity(intent)
+            }
+
+            if(checkflag.equals("year2")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.sndic)
+                startActivity(intent)
+            }
+            if(checkflag.equals("year3")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.trdic)
+                startActivity(intent)
+            }
+            if(checkflag.equals("year4")){
+                val intent=Intent(this, ThirdActivity::class.java)
+                intent.putExtra("picture" , R.drawable.frtip)
+                startActivity(intent)
+            }
 
         }
 
--- app/src/main/java/com/example/campusapp/ThirdActivity.kt
+++ app/src/main/java/com/example/campusapp/ThirdActivity.kt
@@ -1,53 +1,21 @@
 package com.example.campusapp
 
-import android.graphics.Color
-import android.graphics.LinearGradient
-import android.graphics.Shader
-import android.os.Build
 import android.os.Bundle
 import android.widget.ImageView
-import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import java.time.LocalDate
-import java.time.DayOfWeek
-import java.time.LocalTime
-import java.time.Month
-import java.util.*
 
-class ThirdActivity :AppCompatActivity(){
-    @RequiresApi(Build.VERSION_CODES.O)
+class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
 
-        val c=Calendar.getInstance()
-        val date=c.get(Calendar.DATE)
-        val hour=c.get(Calendar.HOUR_OF_DAY)
-        val minute=c.get(Calendar.MINUTE)
-        val dayOfWeek=c.get(Calendar.DAY_OF_WEEK)
-        val day=when(dayOfWeek){
-            1->"MONDAY"
-            2->"TUESDAY"
-            3->"WEDNESDAY"
-            4->"THRUSDAY"
-            5->"FRIDAY"
-            6->"SATURDAY"
-            else->"SUNDAY"
+
+        val imageView : ImageView = findViewById(R.id.imageView5)
+        val bundle = intent.extras
+        if(bundle != null){
+            val picture = bundle.getInt("picture")
+            imageView.setImageResource(picture)
         }
-
-        val textDate:TextView=findViewById(R.id.textDate)
-        val textTime:TextView=findViewById(R.id.textTime)
-        val textDayOfWeek:TextView=findViewById(R.id.textDayOfWeek)
-        textDate.text= date.toString()
-        textTime.text= "$hour:$minute"
-        textDayOfWeek.text=day
-
-        val image:String? =intent.getStringExtra("flag")
-
-        val imageViewZoomInOut:ImageView=findViewById(R.id.imageViewZoomInOut)
-        Glide.with(this).load(image).error(R.drawable.image1).into(imageViewZoomInOut)
 
     }
 }
